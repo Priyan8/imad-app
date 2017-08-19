f80afe1c3dcd3bf7f385238426d9bb11ed01c373
Priyan8 <30595465+Priyan8@users.noreply.github.com>
2017-08-19 10:55:49 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 qpp.get('/counter', function(req, res){
     counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
